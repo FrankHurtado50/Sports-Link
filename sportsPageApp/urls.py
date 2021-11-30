1f38f6186abd1c3e15481bcf6c59939b48403fc5
FrankHurtado50 <frankhurtado50@gmail.com>
2021-11-29 19:09:28 -0800

    third commit

--- sportsPageApp/urls.py
+++ sportsPageApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('register', views.register_info),
+    path('dashboard', views.dashboard),
 ]
--- sportsPageApp/views.py
+++ sportsPageApp/views.py
@@ -6,7 +6,7 @@ from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "reg_page.html")
 
 
 def register_info(request):
@@ -29,3 +29,26 @@ def register_info(request):
 
         request.session['user_id'] = new_account.id
         return redirect('/dashboard')
+
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/")
+    #if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+    else:
+        user = User.objects.get(email = request.POST['email'])
+        request.session['user_id'] = user.id
+        return redirect('/dashboard')
+
+
+def dashboard(request):
+    if "user_id" not in request.session:
+        return redirect("/")
+    context = {
+        "logged_in_user": User.objects.get(id = request.session['user_id']),
+        # add more context
+    }
+    return render(request, "dashboard.html", context)
